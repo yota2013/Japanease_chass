4e6f64cd6145b1e24866bcb35ae9916ecf720a15
yota2013 <yota2013@aimosakaguest07@gmail.com>
2015-09-08 19:00:53 +0900

    2日の進捗

--- Assets/Script/Login_user_take.cs
+++ Assets/Script/Login_user_take.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 using MiniJSON;
 
 public class Login_user_take : MonoBehaviour {
-	string url;
 	string www_take_text;
 	public string user_id{ get; private set;}
 	public string play_id{ get; private set;}
 	public string state{ get; private set;}
 	public string role{ get; private set;}
-
+	public string username{ get; private set;}
+	public string url{ get; private set;}
+	public string room{ get; private set;}
 	//string Json_text;
 	//public InputField Input_data;
 
@@ -23,20 +24,35 @@ public class Login_user_take : MonoBehaviour {
 	void Update () {
 	
 	}
-	public void OnClick_InputFiled(Text Input_data){
-		url = Input_data.text;
+
+	public void setroom(Text room)
+	{
+		 this.room = room.text;
+	}
+	public void setusername(Text username)
+	{
+		 this.username = username.text;
+	}
+	public void seturl(Text url)
+	{
+		this.url = url.text;
+	}
+
+	public void OnClick_InputFiled(){
 		url = "http://192.168.33.11:3000/users/login";
-		//Debug.Log (url);
+		Debug.Log (url);
+		Debug.Log (username);
+		Debug.Log (room);
 		//Login_User_Take ();
 		StartCoroutine(Login_User_Take());
 	}
 
 	private IEnumerator Login_User_Take()
 	{
-		Debug.Log ("OK");
+
 		WWWForm Form = new WWWForm ();
-		Form.AddField ("name","1");
-		Form.AddField( "room_no", "1" );
+		Form.AddField ("name",username);
+		Form.AddField( "room_no",room);
 		WWW www = new WWW(url, Form);
 		yield return www;
 		www_take_text = www.text;
@@ -51,7 +67,6 @@ public class Login_user_take : MonoBehaviour {
 		user_id = json_analys_text["user_id"] as string;
 		state = json_analys_text["state"] as string;
 		user_id = json_analys_text["role"]as string;
-
 	}
 		
 
